8749100b5647799d7855f2288f61f3587c1d1f00
Santoragi32 <woojj1254577@naver.com>
2025-06-20 18:37:15 +0900

    section2: H2 데이터베이스 설치 및 JPA와 DB 설정, 동작 확인

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUserName("MemberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        assertThat(findMember).isEqualTo(member);
+
+        System.out.println("findMember == member : " +(findMember == member));
+    }
+}
