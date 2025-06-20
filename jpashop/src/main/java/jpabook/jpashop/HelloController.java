99c94ae20b8685e9f7a428f071c1faf5e1d83905
Santoragi32 <woojj1254577@naver.com>
2025-06-20 17:33:10 +0900

    section2: View 환경 설정

---
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello"; // resources/templates 안에 있는 .html 파일을 리턴
+    }
+}
--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		//lombok 적용 확인
+//		Hello hello = new Hello();
+//		hello.setData("Hello");
+//		String data = hello.getData();
+//		System.out.println("data: " + data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
